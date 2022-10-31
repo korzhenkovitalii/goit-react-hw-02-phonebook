c1a75e5fbda3a7af1b1eeb03c13c5aa0dcaa52d6
korzhenkovitalii <korzhenko.vitalii@gmail.com>
2022-10-31 10:04:27 +0200

    add form component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,18 +1,22 @@
 import React from 'react';
-import { ContactForm } from './ContactForm/ContactForm';
+import ContactForm from './ContactForm/ContactForm';
 import { ContactList } from './ContactList/ContactList';
 import { Filter } from './Filter/Filter';
 
 class App extends React.Component {
   state = {
     contacts: [],
-    name: '',
   };
+
+  formSubmitHandler(name) {
+    console.log(name);
+  }
+
   render() {
     return (
       <>
         <h1>Phonebook</h1>
-        <ContactForm />
+        <ContactForm onSubmit={this.formSubmitHandler} />
         <h2>Contacts</h2>
         <Filter />
         <ContactList />
--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -1,5 +1,50 @@
 // import css from 'components/ContactForm/ContactForm.module.css'
+import React from 'react';
 
-export const ContactForm = () => {
-    
+class ContactForm extends React.Component {
+  state = {
+    name: '',
+  };
+
+  handleNameChange = e => {
+    // console.log(e.target.name);
+    const { name, value } = e.target;
+
+    this.setState({ [name]: value });
+  };
+
+  handleSubmit = e => {
+    e.preventDefault();
+
+    this.props.onSubmit(this.state.name);
+
+    this.resetForm();
+  };
+
+  resetForm() {
+    this.setState({ name: '' });
+  }
+
+  render() {
+    return (
+      <form onSubmit={this.handleSubmit}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            pattern="^[a-zA-Zа-яА-Я]+(([' -][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
+            title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
+            required
+            value={this.state.name}
+            onChange={this.handleNameChange}
+          />
+        </label>
+
+        <button type="submit">Add contact</button>
+      </form>
+    );
+  }
 }
+
+export default ContactForm;
